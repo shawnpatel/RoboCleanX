fd234a100c1ee7f56dd7d878482124addbec1e98
Shawn Patel <shawnpatel@berkeley.edu>
2021-01-23 15:47:20 -0800

    contours

--- camera_stream.py
+++ camera_stream.py
@@ -73,16 +73,8 @@ class CameraStream():
                     self.old_terminate()
                     break
                 else:
-                    hsv_frame = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-
-                    lower_red = np.array([6, 100, 50])
-                    upper_red = np.array([172, 165, 100])
-                    
-                    mask_frame = cv.bitwise_not(cv.inRange(hsv_frame, lower_red, upper_red))
-
-                    result_frame = cv.bitwise_and(frame, frame, mask=mask_frame)
-
-                    buffer = cv.imencode(".jpg", result_frame)[1]
+                    frame = self.__process_image(frame)
+                    buffer = cv.imencode(".jpg", frame)[1]
                     frame = buffer.tobytes()
 
                     yield (b'--frame\r\n' +
@@ -90,6 +82,49 @@ class CameraStream():
         except GeneratorExit:
             print("Generator closed!")
             self.old_terminate()
+    
+    def __is_bad_contour(self, contour):
+        area = cv.contourArea(contour)
+        peri = cv.arcLength(contour, True)
+        approx_poly = cv.approxPolyDP(contour, 0.02 * peri, True)
+        rotated_rect = rect = cv.minAreaRect(contour)
+
+        if area < 500:
+            return True
+        elif len(approx_poly) > 10:
+            return True
+        elif rotated_rect.angle > 5 or rotatted_rect.angle < -5:
+            return True
+        
+        return False
+
+    def __process_image(self, frame):
+        hsv_frame = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+
+        lower_red = np.array([6, 0, 0])
+        upper_red = np.array([172, 255, 255])
+        
+        hsv_mask = cv.bitwise_not(cv.inRange(hsv_frame, lower_red, upper_red))
+
+        contours, hierarchy = cv.findContours(hsv_mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+        bad_contours_mask = np.ones(frame.shape[:2], dtype="uint8") * 255
+
+        bad_contours = []
+        good_contours = []
+        for contour in contours:
+            if self.__is_bad_contour(contour):
+                bad_contours.append(contour)
+            else:
+                good_contours.append(contour)
+                
+        cv.drawContours(bad_contours_mask, bad_contours, -1, 0, -1)
+
+        result_frame = cv.bitwise_and(frame, frame, mask=hsv_mask)
+        result_frame = cv.bitwise_and(result_frame, result_frame, mask=bad_contours_mask)
+
+        cv.drawContours(result_frame, good_contours, -1, (0,255,0), 1)
+
+        return result_frame
     
     def terminate(self):
         self.stop_thread = True
